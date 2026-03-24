ba13813d0223ad8cbd09fcdb651dc72380345a69
jonnyhall163 <hello@growviadigital.co.uk>
2026-03-24 13:04:37 +0000

    GA4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import Script from 'next/script'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-NP3BRQ4P5T"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NP3BRQ4P5T');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp
